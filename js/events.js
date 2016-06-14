e3affcdf7d4d44ea00e18a50a9f1f43b7c2ff1f5
beso9595 <beso35@live.com>
2016-06-15 00:51:35 +0400

    Tic tac toe game with minimax algorithm

---
+++ js/events.js
@@ -0,0 +1,46 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    //
+    var firstRadioButton = document.getElementsByName("first");
+    firstRadioButton[0].addEventListener("click", function () {
+        first = true;  //setting computer as a first player
+        turn = first;
+    });
+    firstRadioButton[1].addEventListener("click", function () {
+        first = false;  //setting myself as a first player
+        turn = first;
+    });
+    ////
+    var board = [];
+    for (var i = 0; i < 9; i++) {
+        board.push(document.getElementById("box_" + (i + 1)));
+    }
+    //
+    var startButton = document.getElementsByName("start")[0];
+    startButton.addEventListener("click", function () {
+        for (var i = 0; i < 9; i++) {
+            board[i].addEventListener("click", function () {
+                if (!finished) {
+                    var res = changeImg(this.id);
+                    if (res !== false) {
+                        calculate(currentSelf, (first ? element : reverseSymbol(element)), first);
+                    }
+                }
+            });
+        }
+        disableAll();
+        launchBoard(true);
+        if (first) {
+            calculate(currentSelf, element, true);
+        }
+    });
+    //
+    document.getElementsByName("new")[0].addEventListener("click", function () {
+        location.reload();
+    });
+    //
+    function disableAll() {
+        firstRadioButton[0].disabled = true;
+        firstRadioButton[1].disabled = true;
+        startButton.disabled = true;
+    }
+});
